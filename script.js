bbb0ef3db76ffa4a5abb3ba6f2707961cb4c2c17
ehmaddd <ehmaddd@gmail.com>
2023-06-01 15:23:54 +0500

    Add Changes

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const projects = [
   {
+    image: ['./images/proj-2-1.png', './images/proj-2-2.png', './images/proj-2-3.png', './images/proj-2-4.png'],
+    title: 'Redux Coordinate Fetch App',
+    languages: ['REACT', 'REDUX', 'CSS3'],
+    details: "Redux Coordinate Fetch App is a website working with the real live data from AccuWeather API to build a web application for the use of general public that provide general info services. This application will allow users to view the coordinates of their required province.",
+    liveVersion: 'https://react-coordinate-app.onrender.com/',
+    linkSource: 'https://github.com/ehmaddd/react-coordinate-app',
+  },
+  {
+    image: ['./images/proj-4-1.png', './images/proj-4-2.png', './images/proj-4-3.png', './images/proj-4-4.png'],
+    title: 'Space Travelers Hub',
+    languages: ['REACT', 'REDUX', 'CSS3'],
+    details: "Space Traveler's Hub is a website that working with the real live data from SpaceX API to build a web application for a company that provides commercial and scientific space travel services. This application will allow users to book rockets and join selected space missions.",
+    liveVersion: 'https://react-redux-group-project-green.vercel.app/',
+    linkSource: 'https://github.com/ehmaddd/space-travelers-hub',
+  },
+  {
+    image: ['./images/proj-5-1.png', './images/proj-5-2.png', './images/proj-5-3.png', './images/proj-5-4.png'],
+    title: 'Math Magician',
+    languages: ['REACT', 'ES6', 'CSS3'],
+    details: "Math Magician is a Single Page Application which allows the user to have a real-time virtual interface of scientific calculator. It will allow the users to perform basic mathematical functions with the ease of quick access.",
+    liveVersion: 'https://math-magician-k8i5.onrender.com/',
+    linkSource: 'https://github.com/ehmaddd/math-magician',
+  },
+  {
+    image: ['./images/proj-6-1.png', './images/proj-6-2.png', './images/proj-6-3.png', './images/proj-6-4.png'],
+    title: 'Bookstore',
+    languages: ['REACT', 'CSS3', 'HTML5'],
+    details: "Book Store is a Single Page Application which allows the user to have a detailed information about a book library as well as the book reading status. It allows user to keep track of a book read by anyone and also change the particulars and also deleting the book.",
+    liveVersion: 'https://book-store-modj.onrender.com/',
+    linkSource: 'https://github.com/ehmaddd/bookstore',
+  },
+  {
+    image: ['./images/proj-3-1.png', './images/proj-3-2.png', './images/proj-3-3.png', './images/proj-3-4.png'],
+    title: 'Movie Show',
+    languages: ['ES6', 'CSS3', 'HTML5'],
+    details: "A website that allows users to interact with movies show information by extracting it from an API, like or comment them. We achieved that by using JavaScript objects , arrays and API. We also dynamically modified the DOM and added basic events",
+    liveVersion: 'https://ehmaddd.github.io/Movie-Show-API-Based/dist/',
+    linkSource: 'https://github.com/ehmaddd/Movie-Show-API-Based',
+  },
+  {
     image: ['./images/proj-1-1.png', './images/proj-1-2.png', './images/proj-1-3.png', './images/proj-1-4.png'],
     title: 'Global Summit 2015',
     languages: ['HTML5', 'CSS3', 'ES6'],
     details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
     liveVersion: 'https://ehmaddd.github.io/online-conference/',
     linkSource: 'https://github.com/ehmaddd/online-conference',
-  },
-  {
-    image: './images/proj-2.jpg',
-    title: 'Multi-Post Stories Gain + Glory',
-    languages: ['Ruby', 'HTML', 'CSS', 'PHP'],
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
-  },
-  {
-    image: './images/proj-3.jpg',
-    title: 'Multi-Post Stories Gain + Glory',
-    languages: ['Ruby', 'HTML', 'CSS', 'PHP'],
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
-  },
-  {
-    image: './images/proj-4.jpg',
-    title: 'Multi-Post Stories Gain + Glory',
-    languages: ['Ruby', 'HTML', 'CSS', 'PHP'],
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
-  },
-  {
-    image: './images/proj-5.jpg',
-    title: 'Multi-Post Stories Gain + Glory',
-    languages: ['Ruby', 'HTML', 'CSS', 'PHP'],
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
-  },
-  {
-    image: './images/proj-6.jpg',
-    title: 'Multi-Post Stories Gain + Glory',
-    languages: ['Ruby', 'HTML', 'CSS', 'PHP'],
-    details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release.",
-    liveVersion: 'https://ehmaddd.github.io',
-    linkSource: 'https://github.com/ehmaddd/ehmaddd.github.io',
   },
 ];
 
@@ -56,8 +56,12 @@ const modalUl = document.querySelector('#myModal ul');
 
 const works = document.querySelectorAll('.work-sample');
 for (let a = 0; a < works.length; a += 1) {
-  if (a === 0) {
     let count = 0;
+    let interval;
+    if(a===0 || a===3)
+    interval = 3500;
+    else
+    interval = 3500+(a*300);
     setInterval(() => {
       document.querySelectorAll('.work-sample img')[a].src = projects[a].image[count];
       if (count < 3) {
@@ -65,10 +69,7 @@ for (let a = 0; a < works.length; a += 1) {
       } else {
         count = 0;
       }
-    }, 2000);
-  } else {
-    document.querySelectorAll('.work-sample img')[a].src = projects[a].image;
-  }
+    }, interval);
 
   document.querySelectorAll('.work-sample h4')[a].innerHTML = projects[a].title;
   worksUl = document.querySelectorAll('.work-sample ul');
@@ -92,7 +93,6 @@ for (let a = 0; a < btn.length; a += 1) {
 
     //document.querySelector('#myModal img').src = projects[a].image;
     //------------------
-    if (a === 0) {
       let count = 0;
       setInterval(() => {
         document.querySelector('#myModal img').src = projects[a].image[count];
@@ -101,10 +101,7 @@ for (let a = 0; a < btn.length; a += 1) {
         } else {
           count = 0;
         }
-      }, 2000);
-    } else {
-      document.querySelector('#myModal img').src = projects[a].image;
-    }
+      }, 3500);
     //------------------
 
     document.querySelector('#myModal p').innerHTML = projects[a].details;
